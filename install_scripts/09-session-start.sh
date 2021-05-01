093d779dfd9e15523d66bdc63cd0f97e1b8d38cc
Thomas Maurice <thomas@maurice.fr>
2021-05-01 10:19:22 +0100

    Composeable startup script

--- install_scripts/09-session-start.sh
+++ install_scripts/09-session-start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cp session-start.sh ~/.session-start.sh
+if ! [ -d ~/autostart.d ]; then mkdir ~/autostart.d; fi;
+touch ~/autostart.d/placeholder.sh
--- session-start.sh
+++ session-start.sh
@@ -7,3 +7,15 @@ xrdb -merge ~/.Xresources
 
 xset +fp ${HOME}/.fonts
 xset fp rehash
+
+cat >> ~/autostart.d/log <<EOF
+
+# New log starting at $(date) #####################################
+
+EOF
+
+if [ -d ~/autostart.d ]; then
+    for f in ~/autostart.d/*.sh; do
+        source "${f}" >> ~/autostart.d/log
+    done;
+fi;
