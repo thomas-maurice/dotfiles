7728f9b10130d5fa31bfab5aef94656c21ddd728
Thomas Maurice <thomas@maurice.fr>
2021-05-01 10:45:41 +0100

    oops

--- install_scripts/14-google-stuff.sh
+++ install_scripts/14-google-stuff.sh
@@ -8,4 +8,4 @@ sudo dpkg -i /tmp/chrome.deb
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get install -y apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo -y apt-get install google-cloud-sdk google-cloud-sdk-datastore-emulator google-cloud-sdk-firestore-emulator
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk google-cloud-sdk-datastore-emulator google-cloud-sdk-firestore-emulator
