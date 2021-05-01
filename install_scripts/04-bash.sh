31b57292c624640d8fe181eca152370aa018d3fd
Thomas Maurice <thomas@maurice.fr>
2021-05-01 10:40:03 +0100

    install asdf plugins

--- install_scripts/04-bash.sh
+++ install_scripts/04-bash.sh
@@ -9,6 +9,8 @@ if ! [ -d ~/.asdf ]; then git clone https://github.com/asdf-vm/asdf.git ~/.asdf 
 
 cp tool-versions ~/.tool-versions
 
+for plugin in $(awk '{ print $1}' ~/.tool-versions); do asdf plugin-add ${plugin}; done
+
 (cd ~ && asdf install)
 
 cp bashrc ~/.bashrc
