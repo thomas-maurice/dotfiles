08cac2fcf656a918147ba9994a8e945c0115ce1d
Thomas Maurice <thomas@maurice.fr>
2021-05-01 15:22:02 +0100

    sudo

--- install_scripts/06-fonts.sh
+++ install_scripts/06-fonts.sh
@@ -3,6 +3,6 @@
 if ! [ -d ~/.compile/siji-font ]; then git clone https://github.com/stark/siji ~/.compile/siji-font; fi
 (cd ~/.compile/siji-font && ./install.sh )
 
-if [ -f /etc/fonts/conf.d/70-no-bitmaps.conf ]; then rm /etc/fonts/conf.d/70-no-bitmaps.conf; fi
+if [ -f /etc/fonts/conf.d/70-no-bitmaps.conf ]; then sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf; fi
 
 sudo fc-cache -f -v
