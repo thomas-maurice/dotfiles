7daa3adcddee5f99c5b4b92c94d705f4fc48d4dd
Thomas Maurice <thomas@maurice.fr>
2021-05-01 10:36:28 +0100

    install tools via asdf

--- install_scripts/04-bash.sh
+++ install_scripts/04-bash.sh
@@ -4,5 +4,12 @@ sudo apt-get install -y bash
 
 if ! [ -d ~/.asdf ]; then git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0; fi
 
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+
+cp tool-versions ~/.tool-versions
+
+(cd ~ && asdf install)
+
 cp bashrc ~/.bashrc
 cp -r bashrc.d ~
