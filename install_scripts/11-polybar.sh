7291f4a200951d53e57bfbb21ada28ba3944a510
Thomas Maurice <thomas@maurice.fr>
2021-05-01 10:13:23 +0100

    Clone polybar only if required

--- install_scripts/11-polybar.sh
+++ install_scripts/11-polybar.sh
@@ -8,14 +8,14 @@ sudo apt-get install -y \
     libxcb-randr0-dev libxcb-composite0-dev \
     python3-xcbgen xcb-proto \
     libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
-    
+
 sudo apt-get install -y \
     libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev \
     libasound2-dev libpulse-dev i3-wm \
     libjsoncpp-dev libmpdclient-dev \
     libcurl4-openssl-dev libnl-genl-3-dev
 
-git clone --recursive https://github.com/polybar/polybar ~/.compile/polybar
+if ! [ -d ~/.compile/polybar ]; then git clone --recursive https://github.com/polybar/polybar ~/.compile/polybar; fi
 (cd ~/.compile/polybar && ./build.sh -A --all-features -f -C -g -j 4)
 
 if ! [ -d ~/.config/polybar ]; then mkdir ~/.config/polybar; cp /usr/local/share/doc/polybar/config ~/.config/polybar; fi
