033fa27a4b0df53a517335ccdf11d80cf0ad4e7f
Thomas Maurice <thomas@maurice.fr>
2021-05-02 08:22:11 +0100

    Adds ssh config

--- install_scripts/13-misc.sh
+++ install_scripts/13-misc.sh
@@ -4,3 +4,6 @@ pip3 install pew
 
 # For arduino stuff
 sudo usermod -a -G dialout thomas
+
+# Adds the directive to the ssh config to remember the keys
+grep -qxF 'AddKeysToAgent yes' ~/.ssh/config || echo 'AddKeysToAgent yes' >> ~/.ssh/config
