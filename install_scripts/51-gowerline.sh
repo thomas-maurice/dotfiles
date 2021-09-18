dd9c533084261a10e00b4bc62a5fd208e55f513c
Thomas Maurice <thomas@maurice.fr>
2021-09-18 14:44:10 +0200

    Installs gowerline via pip

--- install_scripts/51-gowerline.sh
+++ install_scripts/51-gowerline.sh
@@ -4,3 +4,4 @@ if ! [ -d ~/gowerline/src ]; then git clone https://github.com/thomas-maurice/go
 else rm -rf ~/gowerline/src; git clone https://github.com/thomas-maurice/gowerline ~/gowerline/src; fi
 
 (cd ~/gowerline/src && make install-full)
+pip3 install --upgrade gowerline
