333644a2ccb2a216cbc986f7fd5cb12165b64aa9
Thomas Maurice <thomas@maurice.fr>
2021-09-18 14:43:40 +0200

    Installs gowerline

---
+++ install_scripts/51-gowerline.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if ! [ -d ~/gowerline/src ]; then git clone https://github.com/thomas-maurice/gowerline ~/gowerline/src;
+else rm -rf ~/gowerline/src; git clone https://github.com/thomas-maurice/gowerline ~/gowerline/src; fi
+
+(cd ~/gowerline/src && make install-full)
