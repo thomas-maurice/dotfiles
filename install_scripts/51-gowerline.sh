885390c743603538890fc89bf91e190e32199853
Thomas Maurice <thomas@maurice.fr>
2021-09-22 14:32:50 +0200

    Fix the gowerline install

--- install_scripts/51-gowerline.sh
+++ install_scripts/51-gowerline.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if ! [ -d ~/gowerline/src ]; then git clone https://github.com/thomas-maurice/gowerline ~/gowerline/src;
-else rm -rf ~/gowerline/src; git clone https://github.com/thomas-maurice/gowerline ~/gowerline/src; fi
-
-(cd ~/gowerline/src && make install-full)
-pip3 install --upgrade gowerline
+curl -sSfL https://raw.githubusercontent.com/thomas-maurice/gowerline/master/install.sh | bash
