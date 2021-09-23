a23ec978404ce40824fd8b7b90b2f4d3a3ab6c2f
Thomas Maurice <thomas@maurice.fr>
2021-09-23 16:10:00 +0200

    Installs my vscode extensions list

--- install_scripts/50-snaps.sh
+++ install_scripts/50-snaps.sh
@@ -5,6 +5,7 @@ sudo snap install --classic code
 # vscode config
 mkdir -p ~/.config/Code/User || true
 cp vscode/User/settings.json ~/.config/Code/User
+cat vscode-extensions | xargs -L 1 code --install-extensions
 
 sudo snap install --stable spotify
 
