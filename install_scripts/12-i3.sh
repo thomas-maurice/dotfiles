07961c577d20632ba561f6e84f6b0d2775b3c227
Thomas Maurice <thomas@maurice.fr>
2022-02-09 14:11:38 +0000

    moar

--- install_scripts/12-i3.sh
+++ install_scripts/12-i3.sh
@@ -12,7 +12,7 @@ if ! [ -d ~/.config/i3 ]; then mkdir ~/.config/i3; fi
 cp i3/config ~/.config/i3
 
 # install rofi themes
-git clone https://github.com/adi1090x/rofi ~/.rofi-themes
+if ! [ -d ~/.rofi-themes ]; then git clone https://github.com/adi1090x/rofi ~/.rofi-themes; fi
 (cd ~/.rofi-theme; echo 1 | ./setup.sh )
 
 fi;
